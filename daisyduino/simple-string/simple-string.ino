43ffce7adff573a640ca14cf4e16bf15c5ced352
Vlad <bleeptools@icloud.com>
2023-04-18 16:24:18 +0200

    Use button

--- daisyduino/simple-string/simple-string.ino
+++ daisyduino/simple-string/simple-string.ino
@@ -5,8 +5,10 @@
 #include <algorithm>
 
 static DaisyHardware seed;
-static StringOsc str;
+static StringOsc string;
 
+// Define note frequencies in Hz
+// C4 is a "middle C" on the keyboard
 #define C4 261.63f
 #define D4 293.66f
 #define E4 329.63f
@@ -16,51 +18,57 @@ static StringOsc str;
 #define B4 493.88f
 #define C5 523.25f
 
-const int kArpegioLength = 3;
-const float kArpeggio[kArpegioLength] = { C4, E4, G4 };
-uint8_t arp_idx;
+// Define note sequence
+const int kNotesCount = 3;
+const float kNotes[kNotesCount] = { C4, E4, G4 };
+uint8_t next_note_index = 0;
 
-const float kTempo = 120;
-const float kSecondsPerMinute = 60;
-uint32_t trigger_interval;
-uint32_t trigger_countdown;
+const int kButtonPin = 2;
+bool button_was_pushed = false;
 
 static void AudioCallback(float **in, float **out, size_t size) {
-  uint8_t excite_string;
+  uint8_t excite_string = 0;
+  
+  // Take the button state
+  bool button_is_pushed = digitalRead(kButtonPin);
+  // Check if the state of the button changed from
+  // released to pushed, and if yes...
+  if (button_is_pushed && !button_was_pushed) {
+    //...then we excite the string...
+    excite_string = 1;
+    //...and play the next note.
+    string.SetFreq(kNotes[next_note_index]);
+   // Then move to the next note. If the note we 
+   // just played was the last in the sequence,
+   // we jump to the first one with "% kNotesCount"
+    next_note_index = (next_note_index + 1) % kNotesCount;
+  }
+  // Save the button state for the next iteration 
+  button_was_pushed = button_is_pushed;
+
+  // Iterate through the number of samples in the buffer...
   for (size_t i = 0; i < size; i++) {
-    excite_string = 0;
-    if (trigger_countdown == 0) {
-      excite_string = 1;
-      str.SetFreq(kArpeggio[arp_idx]);
-      
-      arp_idx = (arp_idx + 1) % kArpegioLength;
-      trigger_countdown = trigger_interval;
-    }
-    trigger_countdown --;
-    
-    out[0][i] = out[1][i] = str.Process(excite_string);
+    // ... and fill both left and right channels 
+    // of the output buffer with String Oscilator output
+    out[0][i] = out[1][i] = string.Process(excite_string);
   }
 }
 
 void setup() {
-  // initialize seed hardware and daisysp modules
-  float sample_rate;
+  // Initialize Daisy Seed hardware
   seed = DAISY.init(DAISY_SEED, AUDIO_SR_48K);
 
-  sample_rate = DAISY.get_samplerate();
+  // Set up String Oscilator
+  float sample_rate = DAISY.get_samplerate();
+  string.Init(sample_rate);
+  string.SetDamping(.8f);
+  string.SetNonLinearity(.1f);
+  string.SetBrightness(.5f);
 
-  float samples_per_beat = kSecondsPerMinute * sample_rate / kTempo;
-  trigger_interval = samples_per_beat / 2; // 1/8th
+  // Setup button
+  pinMode(kButtonPin, INPUT);
 
-  // Set up String algo
-  str.Init(sample_rate);
-  str.SetDamping(.8f);
-  str.SetNonLinearity(.1f);
-  str.SetBrightness(.5f);
-
-  arp_idx = 0;
-
-  // start callback
+  // Start Audio Callback
   DAISY.begin(AudioCallback);
 }
 
