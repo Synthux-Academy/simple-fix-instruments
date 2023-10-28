c6f3cb09e82ba7e61c77bf3b3aa8ed963e28ab24
Vlad <bleeptools@icloud.com>
2023-10-28 22:41:41 +0200

    Speed

--- daisyduino/simple-looper-speed/looper.h
+++ daisyduino/simple-looper-speed/looper.h
@@ -18,7 +18,7 @@ class Looper {
     void Init(float *buf, size_t length) {
         _buffer.Init(buf, length, win_slope);
         _loop_length = length;
-        _Activate(0, 0, length);
+        _Activate(0);
     }
 
     void SetRecording(bool is_rec_on) {
@@ -34,7 +34,11 @@ class Looper {
       if (abs(static_cast<int32_t>(new_start) - static_cast<int32_t>(_loop_start)) > 500) _loop_start = new_start;
       
       auto new_length = static_cast<size_t>(loop_length * _buffer.Length());
-      if (abs(static_cast<int32_t>(new_length) - static_cast<int32_t>(_loop_length)) > 1200) _loop_length = new_length;
+      auto norm_length = (new_length / win_slope) * win_slope;
+      if (new_length - norm_length > win_slope / 2) norm_length += win_slope;
+      if (abs(static_cast<int32_t>(norm_length) - static_cast<int32_t>(_loop_length) > win_slope)) {
+        _loop_length = max(2 * win_slope, norm_length);
+      }
     }
   
     float Process(float in) {
@@ -44,7 +48,7 @@ class Looper {
         float output = 0;
 
         for (auto& w: _wins)
-            if (w.IsHalf()) _Activate(w.PlayHead(), w.LoopStart(), w.LoopLength());
+            if (w.IsHalf()) _Activate(w.PlayHead());
 
         for (auto& w: _wins)
             if (w.IsActive()) output += w.Process(_buffer);
@@ -53,10 +57,10 @@ class Looper {
     }
 
 private:
-    void _Activate(float start, const size_t loop_start, const size_t loop_length) {
+    void _Activate(float play_head) {
        for (auto& w: _wins) {
             if (!w.IsActive()) {
-                w.Activate(start, _delta, _loop_start, _loop_length);
+                w.Activate(play_head, _delta, _loop_start, _loop_length);
                 break;
             }
         }
@@ -65,7 +69,7 @@ private:
     static const size_t kMinLoopLength = 2 * win_slope;
 
     Buffer _buffer;
-    std::array<Window<win_slope>, 4> _wins;
+    std::array<Window<win_slope>, 2> _wins;
 
     float _delta;
     size_t _loop_start;
--- daisyduino/simple-looper-speed/simple-looper-speed.ino
+++ daisyduino/simple-looper-speed/simple-looper-speed.ino
@@ -5,8 +5,8 @@
 // Setup pins
 static const int record_pin      = D(S35);
 static const int loop_start_pin  = A(S33);
-static const int loop_length_pin = A(S34);
-static const int mod_pin         = A(S30);
+static const int loop_length_pin = A(S30);
+static const int loop_speed_pin  = A(S37);
 
 static const float kKnobMax = 1023;
 
@@ -23,8 +23,6 @@ float mod_val = 0;
 
 void AudioCallback(float **in, float **out, size_t size) {
   for (size_t i = 0; i < size; i++) {
-    auto inst_mod = mod.Process();
-    looper.SetSpeed(1.0 + inst_mod * mod_val);
     auto looper_out = looper.Process(in[1][i]);
     out[0][i] = out[1][i] = looper_out;
   }
@@ -48,13 +46,13 @@ void setup() {
 }
 
 void loop() {
-  // Set loop parameters
-  // auto raw_loop_speed = analogRead(loop_speed_pin); 
-  // auto loop_speed = 0.f;
-  // if (raw_loop_speed < 500 || raw_loop_speed > 524) {
-  //   loop_speed = fmap(raw_loop_speed / kKnobMax, -2.f, 2.f);
-  // }
-  // looper.SetSpeed(loop_speed);
+  auto raw_loop_speed = analogRead(loop_speed_pin); 
+  auto loop_speed = 0.f;
+  if (raw_loop_speed < 500 || raw_loop_speed > 524) {
+    loop_speed = fmap(raw_loop_speed / kKnobMax, -2.f, 2.f);
+  }
+  looper.SetSpeed(loop_speed);
+  // Serial.println(loop_speed);
 
   auto loop_start = fmap(analogRead(loop_start_pin) / kKnobMax, 0.f, 1.f);
   auto loop_length = fmap(analogRead(loop_length_pin) / kKnobMax, 0.f, 1.f, Mapping::EXP);
